03b8869b1c507128433dec0746f15c875924588d
angela <yaoliu928@gmail.com>
2022-10-27 20:43:54 +1000

    105&106 finish

--- src/components/AddExpensePage.jsx
+++ src/components/AddExpensePage.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import ExpenseForm from "./ExpenseForm";
 
 const AddExpensePage = () => (
-  <div className="">AddExpensePage
+  <div >
+    <h1>AddExpensePage</h1>
+    <ExpenseForm />
   </div>
 )
 
