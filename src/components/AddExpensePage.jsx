c0737bd8ec2fa0368713ec2bdeacb0460e21f35d
angela <yaoliu928@gmail.com>
2022-11-04 17:51:55 +1000

    124 Testing AddExpensePage only use mapDispatchToProps

--- src/components/AddExpensePage.jsx
+++ src/components/AddExpensePage.jsx
@@ -3,17 +3,25 @@ import { connect } from "react-redux";
 
 import { addExpense } from "../actions/expenses";
 import ExpenseForm from "./ExpenseForm";
+export class AddExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    this.props.addExpense(expense);
+    this.props.history.push('/');
+  }
+  render() {
+    return (
+      <div >
+        <h1>AddExpensePage</h1>
+        <ExpenseForm
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    )
+  }
+}
 
-const AddExpensePage = (props) => (
-  <div >
-    <h1>AddExpensePage</h1>
-    <ExpenseForm
-      onSubmit={(expense) => {
-        props.dispatch(addExpense(expense));
-        props.history.push('/');
-      }}
-    />
-  </div>
-)
+const mapDispatchToProps = (dispatch) => ({
+  addExpense: expense => dispatch(addExpense(expense))
+})
 
-export default connect()(AddExpensePage);
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
