a47e1abeed0306f3e894262f782462e841e3d272
angela <yaoliu928@gmail.com>
2022-11-15 10:46:56 +1000

    143 writing to database use firebase@4.2.0

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import * as firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBn6wFRZ9F7viwuLx_6sDFPMlHai486L24",
@@ -10,4 +10,23 @@ const firebaseConfig = {
   appId: "1:449697607742:web:f23561970c370e176d216f",
   measurementId: "G-LHQ74GK2CN"
 };
-initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+database.ref().set({
+  name: 'Andrew Mead',
+  age: 26,
+  isSingle: false,
+  location: {
+    city: 'Philadelphia',
+    country: 'United States'
+  }
+});
+
+database.ref('age').set(27);
+database.ref('location/city').set('New York');
+database.ref('attributes').set({
+  height: 183,
+  weight: 150
+});
