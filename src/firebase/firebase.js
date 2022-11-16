fcfca1c98316f4a95ab7ac15bb899b362f828497
angela <yaoliu928@gmail.com>
2022-11-16 10:38:04 +1000

    148 fetching data from firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,30 +14,61 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database.ref().set({
-  name: 'Andrew Mead',
-  age: 26,
-  stressLevel: 6,
-  job: {
-    title: 'Software developer',
-    company: 'Google'
-  },
-  isSingle: false,
-  location: {
-    city: 'Philadelphia',
-    country: 'United States'
-  }
-}).then(() => {
-  console.log('Data is saved!');
-}).catch((e) => {
-  console.log('This failed.', e);
+database.ref().on('value', (snapshot) => {
+  const val = snapshot.val();
+  console.log(`${val.name} is a ${val.job.title} at ${val.job.company}.`);
 });
 
-database.ref().update({
-  stressLevel: 9,
-  'job/company': 'Amazon',
-  'location/city': 'Seattle'
-});
+// const onValueChange = database.ref().on('value',
+//   (snapshot) => { console.log(snapshot.val()); },
+//   (e) => { console.log('Error with data fetching', e); })
+
+// setTimeout(() => {
+//   database.ref('age').set(29);
+// }, 3500);
+
+// setTimeout(() => {
+//   database.ref().off('value', onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//   database.ref('age').set(30);
+// }, 10500);
+
+// database.ref('location/city')
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((e) => {
+//     console.log('Error fetching data', e);
+//   })
+
+// database.ref().set({
+//   name: 'Andrew Mead',
+//   age: 26,
+//   stressLevel: 6,
+//   job: {
+//     title: 'Software developer',
+//     company: 'Google'
+//   },
+//   isSingle: false,
+//   location: {
+//     city: 'Philadelphia',
+//     country: 'United States'
+//   }
+// }).then(() => {
+//   console.log('Data is saved!');
+// }).catch((e) => {
+//   console.log('This failed.', e);
+// });
+
+// database.ref().update({
+//   stressLevel: 9,
+//   'job/company': 'Amazon',
+//   'location/city': 'Seattle'
+// });
 
 // database.ref('isSingle').set(null); // Another way to remove
 
@@ -48,6 +79,3 @@ database.ref().update({
 //   .catch((e) => {
 //     console.log('Did not remove data', e);
 //   });
-
-
-
