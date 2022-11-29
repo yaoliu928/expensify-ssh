361792fa4d5b1b79a3a99ecc36c1d2fc98db628b
angela <yaoliu928@gmail.com>
2022-11-29 11:07:49 +1000

    168 public only routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,8 @@ import { startLogout } from "../actions/auth";
 export const Header = ({ startLogout }) => (
   <div>
     <h1>Expensify</h1>
-    <NavLink to='/' activeClassName="is-active" exact>Dashboard </NavLink>
+    <NavLink to='/dashboard' activeClassName="is-active">Dashboard </NavLink>
     <NavLink to='/create' activeClassName="is-active">Create Expenses </NavLink>
-    <NavLink to='/help' activeClassName="is-active">Help </NavLink>
     <button onClick={startLogout}>Logout</button>
   </div>
 );
--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -3,11 +3,11 @@ import { Router, Route, Switch } from "react-router-dom";
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
 import createHistory from 'history/createBrowserHistory';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
@@ -15,11 +15,10 @@ const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <Route path="/" exact component={LoginPage} />
+        <PublicRoute path="/" exact component={LoginPage} />
         <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
         <PrivateRoute path="/create" component={AddExpensePage} />
         <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
