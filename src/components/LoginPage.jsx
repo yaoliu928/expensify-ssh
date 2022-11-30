23d8ce189fb41477d2d9544aa26250acc96e20a8
angela <yaoliu928@gmail.com>
2022-11-30 20:05:06 +1000

    172 styling buttons in Header

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -8,7 +8,9 @@ const LoginPage = ({ startLogin }) => {
       <div className="box-layout__box">
         <h1 className="box-layout__title">Expensify App</h1>
         <p>It's time to get your expenses under control.</p>
-        <button onClick={startLogin}>Login</button>
+        <button
+          className="button"
+          onClick={startLogin}>Login with Google</button>
       </div>
     </div>)
 }
